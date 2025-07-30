4eeb41686bb90b6728ebff31aa0f660d816e427f
SavonnaB <143576792+SavonnaB@users.noreply.github.com>
2025-07-30 20:43:12 +0000

    created recursive class and findMax int and .java file

---
+++ RecursiveExercises.java
@@ -0,0 +1,8 @@
+Public Class RecursiveExercises {    
+    
+    public static int findMax(int[] arr, int left, int right) {
+        if (left == right) {
+            return arr[left];
+        }
+    }
+}
